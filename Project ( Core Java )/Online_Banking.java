93ed5ad3442cc541cbfa9b57eb105f0b4dad0cf9
Suraj Surve <Survesuraj38@gmail.com>
2025-08-28 13:54:32 +0530

    Update Online_Banking.java

--- Project ( Core Java )/Online_Banking.java
+++ Project ( Core Java )/Online_Banking.java
@@ -1,21 +1,40 @@
 import java.util.Scanner;
 
-public class Online_Banking {
-  public static void main(String[] args) {
+class BankProcess implements Runnable {
+    Customer obj;
 
-    Customer obj = new Customer();
-    try {
-
-      obj.UserCheck();
-      obj.Account_type();
-      obj.Process();
-      obj.display();
-
-    } catch (Exception e) {
-      System.out.println("An error occurred: " + e.getMessage());
-    } finally {
-      obj.close();
-      System.out.println("Thank you for visiting .....");
+    public BankProcess(Customer obj) {
+        this.obj = obj;
     }
-  }
+
+    
+    public void run() {
+        try {
+                obj.UserCheck();
+                obj.Account_type();
+                obj.Process();
+                obj.display();
+            
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            System.out.println("Failed to create account ,information is invalid >>> fill information according to the passbook ...");
+        } finally {
+            obj.close();
+            System.out.println("Thank you for visiting .....");
+        }
+    }
+}
+
+public class Online_Banking {
+    public static void main(String[] args) {
+        Customer obj = new Customer();
+        BankProcess ob1 = new BankProcess(obj);
+
+        try {
+            Thread t = new Thread(ob1); 
+            t.start(); 
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 }
